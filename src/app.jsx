77af951b0114f26afaf6839490f83a4a42b2532b
Lucas Amorim <lucas--amorim@outlook.com>
2026-03-27 23:15:52 -0300

    tailwind e css adicionados

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="">Typing Speed Test</h1>
+      {/* Your typing speed test components will go here */}
+    </div>
+  );
+};
+
+export default App;
