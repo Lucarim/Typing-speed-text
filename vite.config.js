fcc46e4890a3e1a20cccfccd67fd50dadede3e53
Lucas Amorim <lucas--amorim@outlook.com>
2026-03-29 13:45:59 -0300

    name att

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/Typing-speed-text/",
+  base: "/Typing-speed-test/",
   plugins: [react(), tailwindcss()],
 });
